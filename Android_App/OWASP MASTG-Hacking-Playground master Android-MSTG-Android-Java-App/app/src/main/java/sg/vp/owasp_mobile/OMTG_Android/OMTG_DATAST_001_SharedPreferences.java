5e6c19251e400b8f49b2c96a94d37c9d432aec5f
JonUnzalu <jonunzalu@yahoo.es>
2023-03-01 21:55:38 +0100

    Preferences android

--- Android_App/OWASP MASTG-Hacking-Playground master Android-MSTG-Android-Java-App/app/src/main/java/sg/vp/owasp_mobile/OMTG_Android/OMTG_DATAST_001_SharedPreferences.java
+++ Android_App/OWASP MASTG-Hacking-Playground master Android-MSTG-Android-Java-App/app/src/main/java/sg/vp/owasp_mobile/OMTG_Android/OMTG_DATAST_001_SharedPreferences.java
@@ -1,14 +1,27 @@
 package sg.vp.owasp_mobile.OMTG_Android;
 
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyProperties;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import static android.content.Context.MODE_WORLD_READABLE;
 
+import androidx.security.crypto.EncryptedSharedPreferences;
+import androidx.security.crypto.MasterKey;
+
+
 public class OMTG_DATAST_001_SharedPreferences extends AppCompatActivity {
 
+    private Toast toast;
+    static final int KEY_SIZE = MasterKey.DEFAULT_AES_GCM_MASTER_KEY_SIZE;
+    static final String MASTER_KEY_ALIAS = MasterKey.DEFAULT_MASTER_KEY_ALIAS;
+    private MasterKey masterKey;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +39,43 @@ public class OMTG_DATAST_001_SharedPreferences extends AppCompatActivity {
 
     }
 
+    @android.support.annotation.RequiresApi(api = Build.VERSION_CODES.M)
+    public void gakoakSortu() {
+        try {
+            KeyGenParameterSpec spec = new KeyGenParameterSpec.Builder(
+                    MASTER_KEY_ALIAS,
+                    KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT)
+                    .setBlockModes(KeyProperties.BLOCK_MODE_GCM)
+                    .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
+                    .setKeySize(KEY_SIZE)
+                    .build();
 
+            masterKey = new MasterKey.Builder(OMTG_DATAST_001_SharedPreferences.this)
+                    .setKeyGenParameterSpec(spec)
+                    .build();
+        } catch (Exception e) {
+            Toast.makeText(this, "Exception " + e.getMessage() + " occured", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @android.support.annotation.RequiresApi(api = Build.VERSION_CODES.M)
+    public void enkriptatu() {
+        try {
+            SharedPreferences sharedPreferences = EncryptedSharedPreferences.create(OMTG_DATAST_001_SharedPreferences.this, "key",
+                    masterKey,
+                    EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+                    EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+            );
+            // storing a value
+            sharedPreferences.edit().putString("username", "izaskun").putString("password", "password").apply();
+            // reading a value
+            TextView helloTextView = (TextView) findViewById(R.id.title);
+            helloTextView.setText(sharedPreferences.getString("username",""));
+        }
+        catch(Exception e) {
+            Toast.makeText(this, "Exception " + e.getMessage() + " occured", Toast.LENGTH_LONG).show();
+        }
+    }
 
 
 }
